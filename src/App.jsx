9250868386b54e80eb4d41b9329b84e1b2399c54
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2023-01-02 23:27:24 -0600

    Add api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
+const app = new Clarifai.App({
+  apiKey: '31f92ddc6edc479b96d4507b33dbded2',
+});
+
 function App() {
+  // create a function that logs the event target value for an input element
+  const logEvent = (event) => {
+    console.log(event.target.value);
+  };
+
+  const onBtnSubmit = () => {
+    app.models
+      .predict(
+        '53e1df302c079b3db8a0a36033ed2d15',
+        'https://samples.clarifai.com/dog2.jpeg'
+      )
+      .then((response) => {
+        console.log('hi', response);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="App">
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm logEvent={logEvent} onBtnSubmit={onBtnSubmit} />
     </div>
   );
 }
--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ logEvent, onBtnSubmit }) => {
   return (
     <div className="tc">
       <p className="f3">
         Detect any face within any picture with Wizard Eyes today!
       </p>
       <div className="pl2 pr2">
-        <input type="text" className="f4 pa2" placeholder="Image URL" />
-        <button className="f4 link ph3 pv2 dib white bg-light-purple pointer">
+        <input
+          type="text"
+          className="f4 pa2"
+          placeholder="Image URL"
+          onChange={logEvent}
+        />
+        <button
+          className="f4 link ph3 pv2 dib white bg-light-purple pointer"
+          onClick={onBtnSubmit}
+        >
           Detect Face
         </button>
       </div>
